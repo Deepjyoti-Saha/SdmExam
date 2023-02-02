6b4be61fdd3b0f6fef65e1b958dc4ba3dc87d5e7
Deepjyoti <deepjyotiaec@gmail.com>
2023-02-02 15:51:35 +0530

    menu program

---
+++ Menu.java
@@ -0,0 +1,22 @@
+public class Menu
+{
+    public static void main(String args[])
+    {
+       int a =4;
+
+       switch(a){
+        case 1:
+        System.out.println("The direction is North");
+
+        case 2:
+        System.out.println("The direction is East");
+
+        case 3:
+        System.out.println("The direction is South");
+
+        case 4:
+        System.out.println("The direction is West");
+
+       }
+    }
+}
